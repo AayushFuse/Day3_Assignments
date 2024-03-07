9bc5cc717f021e2866ba67cb39e3561b48d7c28e
Aayush181509 <aayush.181509@ncit.edu.np>
2024-03-07 11:32:55 +0545

    FILE IO Assignment all completed

---
+++ file_io.py
@@ -0,0 +1,35 @@
+# Implement a program that reads a CSV file named "data.csv," containing columns "Name" and "Age." 
+# Create a new CSV file called "adults.csv" with only the rows of individuals who are 18 years or older.
+
+import pandas as pd
+
+
+info = pd.read_csv('files/data.csv')
+info[info.Age>18].to_csv('files/adults.csv',index=None)
+
+
+
+# Create a function add_to_json that takes a filename and a dictionary as input. 
+# The function should read the JSON data from the file, add the new dictionary to it, and write the updated data back to the same file.
+import json
+
+def add_to_json(filename,in_dict):
+    try:
+        with open(filename,'r') as file:
+            data = json.load(file) 
+        
+        data.append(in_dict)
+        print(data)
+        with open(filename,'w') as file:
+            json.dump(data,file)
+    except Exception as e:
+        print(f"Exception: {e}")
+
+
+
+data ={
+    "name": "Laxman",
+    "age": 20
+  }
+add_to_json('files/file.json',data)
+
