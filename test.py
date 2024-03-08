e14f8544cf9d9749fd444f30afc103135bd25d92
Aayush181509 <aayush.181509@ncit.edu.np>
2024-03-08 17:52:16 +0545

    Just trying out hooks

--- test.py
+++ test.py
@@ -1,14 +1,4 @@
-import csv
+import logging
 
 
-with open('data.csv') as ifile,open('adults.csv','w') as ofile:
-
-    reader = csv.DictReader(ifile) 
-    fields = reader.fieldnames    
-    writer = csv.DictWriter(ofile,fieldnames=fields)
-    writer.writeheader()
-    for row in reader:
-        if int(row['Age'])>20:
-            writer.writerow(row)
-    
-    
+logging.warning("Debug")
